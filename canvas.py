014c640d89d65fce490bda60327bc914f3b23361
Royall Spence <royall@royall.us>
2025-10-24 13:44:32 -0400

    Finish ppm implementation

--- canvas.py
+++ canvas.py
@@ -1,6 +1,15 @@
 import math
 
 
+def clamp(n: float) -> int:
+    v = int(math.ceil(n * 255))
+    if v < 0:
+        return 0
+    elif v > 255:
+        return 255
+    return v
+
+
 class Color:
     r: float
     g: float
@@ -12,9 +21,7 @@ class Color:
         self.b = b
 
     def __add__(self, other) -> "Color":
-        return Color(
-            self.r + other.r, self.g + other.g, self.b + other.b
-        )
+        return Color(self.r + other.r, self.g + other.g, self.b + other.b)
 
     def __eq__(self, other) -> bool:
         return (
@@ -24,32 +31,49 @@ class Color:
         )
 
     def __sub__(self, other) -> "Color":
-        return Color(
-            self.r - other.r, self.g - other.g, self.b - other.b
-        )
+        return Color(self.r - other.r, self.g - other.g, self.b - other.b)
 
     def scale(self, scale: float) -> "Color":
         return Color(self.r * scale, self.g * scale, self.b * scale)
 
     def __mul__(self, other) -> "Color":
-        return Color(
-            self.r * other.r, self.g * other.g, self.b * other.b
-        )
+        return Color(self.r * other.r, self.g * other.g, self.b * other.b)
+
+    def as_ppm(self) -> str:
+        return "{0} {1} {2}".format(clamp(self.r), clamp(self.g), clamp(self.b))
+
+
+def replace_char(s: str, ix: int, char: str) -> str:
+    return s[:ix] + char + s[ix + 1 :]
+
+
+def wrap_line(l: str, n=70) -> str:
+    if len(l) > n:
+        # seek to pos n
+        # step backward from n-1 looking for spaces
+        for i in range(n - 1, 0, -1):
+            if l[i] == " ":
+                l = replace_char(l, i, "\n")
+                return l[: i + 1] + wrap_line(l[i + 1 :], n)
+    return l
+
 
 class Canvas:
     width: int
     height: int
     pixels: list[list[Color]]
+    fill: Color
 
-    def __init__(self, width, height):
+    def __init__(self, width, height, fill=Color(0, 0, 0)):
         self.width = width
         self.height = height
         self.pixels = []
+        self.fill = fill
 
         for x in range(width):
             self.pixels.append([])
             for y in range(height):
-                self.pixels[x].append(Color(0, 0, 0))
+                self.pixels[x].append(self.fill)
 
     def pixel_at(self, x, y) -> Color:
         return self.pixels[x][y]
@@ -58,8 +82,18 @@ class Canvas:
         self.pixels[x][y] = color
 
     def as_ppm(self) -> str:
-        out = list()
-        out.append("P3")
-        out.append(f"{self.width} {self.height}")
-        out.append("255")
-        return "\n".join(out)
+        lines = list()
+        lines.append("P3")
+        lines.append(f"{self.width} {self.height}")
+        lines.append("255")
+
+        for y in range(self.height):
+            l = list()
+            for x in range(self.width):
+                l.append(self.pixel_at(x, y).as_ppm())
+            lines.append(" ".join(l))
+
+        for i, line in enumerate(lines):
+            lines[i] = wrap_line(lines[i])
+
+        return "\n".join(lines) + "\n"
--- test_canvas.py
+++ test_canvas.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from canvas import Canvas, Color
+from canvas import Canvas, Color, wrap_line
 
 
 class TestCanvas(TestCase):
@@ -8,16 +8,16 @@ class TestCanvas(TestCase):
         width = 10
         height = 20
         c = Canvas(width, height)
-        self.assertEqual(Color(0,0,0), c.pixel_at(0,0))
-        self.assertEqual(Color(0,0,0), c.pixel_at(width-1,height-1))
+        self.assertEqual(Color(0, 0, 0), c.pixel_at(0, 0))
+        self.assertEqual(Color(0, 0, 0), c.pixel_at(width - 1, height - 1))
 
     def test_write_pixel(self):
         width = 10
         height = 20
-        red = Color(1,0,0)
+        red = Color(1, 0, 0)
         c = Canvas(width, height)
-        c.write_pixel(2,3, red)
-        self.assertEqual(Color(1,0,0), c.pixel_at(2,3))
+        c.write_pixel(2, 3, red)
+        self.assertEqual(Color(1, 0, 0), c.pixel_at(2, 3))
 
     def test_ppm_header(self):
         c = Canvas(5, 3)
@@ -26,3 +26,41 @@ class TestCanvas(TestCase):
         self.assertEqual("P3", lines[0])
         self.assertEqual("5 3", lines[1])
         self.assertEqual("255", lines[2])
+
+    def test_ppm_body(self):
+        c = Canvas(5, 3)
+        c1 = Color(1.5, 0, 0)
+        c2 = Color(0, 0.5, 0)
+        c3 = Color(-0.5, 0, 1)
+        c.write_pixel(0, 0, c1)
+        c.write_pixel(2, 1, c2)
+        c.write_pixel(4, 2, c3)
+        ppm = c.as_ppm()
+        lines = ppm.split("\n")
+        line = lines[3].split(" ")
+        self.assertEqual("255", line[0])
+        self.assertEqual("0", line[1])
+
+        line = lines[4].split(" ")
+        self.assertEqual("128", line[7])
+
+        line = lines[5].split(" ")
+        self.assertEqual("255", line[-1])
+
+    def test_ppm_line_length(self):
+        c = Canvas(10, 2, fill=Color(1, 0.8, 0.6))
+        lines = c.as_ppm().split("\n")
+        self.assertLessEqual(len(lines[3]), 70)
+
+
+class TestWrap(TestCase):
+    def test_wrap_line(self):
+        out = wrap_line("12 34 56 78 90", 6)
+        self.assertEqual("12 34\n56 78\n90", out)
+
+    def test_wrap_line_long(self):
+        out = wrap_line(
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153",
+            70,
+        )
+        self.assertLessEqual(len(out.split("\n")[0]), 70)
